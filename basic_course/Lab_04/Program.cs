7b9679afdc1e06acdacb5e613e51b263beee743e
kriskensy <kriskensy@proton.me>
2024-06-02 16:46:13 +0200

    Lab_4, Lab_5. Simple examples: abstract classes, interfaces, polymorphism, inheritance, overriding, overwriting, operator overloading.

---
+++ basic_course/Lab_04/Program.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+
+// //======================zad 1
+// namespace Lab_04
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Student student = new Student("Nowak", 23, 5468);
+//             student.displayData();
+//         }
+//     }
+//
+//     public abstract class Person
+//     {
+//         public string name { get; set; }
+//         public int age { get; set; }
+//
+//         protected Person(string name, int age)
+//         {
+//             this.name = name;
+//             this.age = age;
+//         }
+//     }
+//
+//     public class Student : Person
+//     {
+//         public int StudentID { get; set; }
+//
+//         public void displayData() => Console.WriteLine($"Student: {name}, age: {age}, ID: {StudentID}");
+//
+//         public Student(string name, int age, int studentId) : base(name, age)
+//         {
+//             StudentID = studentId;
+//         }
+//     }
+// }
+
+// //======================zad 2
+// namespace Lab_04
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Rectangle rectangle = new Rectangle(4, 5);
+//             rectangle.CalculateArea();
+//             Console.WriteLine($"Rectangle area: {rectangle.Area}");
+//
+//             Circle circle = new Circle(5);
+//             circle.CalculateArea();
+//             Console.WriteLine($"Circle area: {circle.Area}");
+//         }
+//     }
+//
+//     public abstract class Shape
+//     {
+//         public double Area { get; set; }
+//
+//         public abstract void CalculateArea();
+//     }
+//
+//     public class Rectangle : Shape
+//     {
+//         private int lenght;
+//         private int width;
+//
+//         public Rectangle(int lenght, int width)
+//         {
+//             this.lenght = lenght;
+//             this.width = width;
+//         }
+//
+//         public override void CalculateArea()
+//         {
+//             Area = width * lenght;
+//         }
+//     }
+//
+//     public class Circle : Shape
+//     {
+//         private int radius;
+//
+//         public Circle(int radius)
+//         {
+//             this.radius = radius;
+//         }
+//
+//         public override void CalculateArea()
+//         {
+//             Area = Math.PI * radius * radius;
+//         }
+//     }
+// }
+
+//====================zad 7
+namespace Lab_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public interface IBook
+    {
+        string Title { get; }
+        string Author { get; }
+        public DateTime PublicationDate { get; }
+        public decimal Price { get; }
+        public string Description { get; set; }
+        public string ToString();
+    }
+
+    public class Book : IBook
+    {
+        public string Title { get; }
+        public string Author { get; }
+        public DateTime PublicationDate { get; }
+        public decimal Price { get; }
+        public string Description { get; set; }
+
+        public Book(string title, string author, DateTime publicationDate, decimal price, string description)
+        {
+            Title = title;
+            Author = author;
+            PublicationDate = publicationDate;
+            Price = price;
+            Description = description;
+        }
+    }
+
+    public class OtherBook : IBook
+    {
+        public int NumberOfPages { get; }
+        public string Title { get; }
+        public string Author { get; }
+        public DateTime PublicationDate { get; }
+        public decimal Price { get; }
+        public string Description { get; set; }
+
+        public OtherBook(int numberOfPages, string title, string author, DateTime publicationDate, decimal price,
+            string description)
+        {
+            NumberOfPages = numberOfPages;
+            Title = title;
+            Author = author;
+            PublicationDate = publicationDate;
+            Price = price;
+            Description = description;
+        }
+    }
+
+    public class Library : ILibrary
+    {
+        public List<IBook> Books { get; }
+
+        public Library()
+        {
+            Books = new List<IBook>();
+        }
+
+        public void AddBook(IBook book)
+            => Books.Add(book);
+
+        public void RemoveBook(IBook book)
+            => Books.Remove(book);
+
+        public void UpdateBook(IBook oldBook, IBook newBook)
+        {
+            var index = Books.IndexOf(oldBook);
+            if (index >= 0)
+                Books[index] = newBook;
+        }
+    }
+
+    public interface ILibrary
+    {
+        List<IBook> Books { get; }
+        void AddBook(IBook book);
+        void RemoveBook(IBook book);
+        void UpdateBook(IBook oldBook, IBook newBook);
+        public string ToString();
+    }
+}
---
+++ basic_course/Lab_05/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+
+// //====================wstep
+// namespace Lab_05
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             var k1 = new Kwota(100,"PLN");
+//             var k2 = new Kwota(100,"PLN");
+//
+//             //var eq = k1 == k2;
+//             var eq = k1 + k2;
+//         }
+//     }
+//
+//     public class Kwota
+//     {
+//         public decimal wartosc { get; private set; }
+//         public string waluta { get; private set; }
+//
+//         public Kwota(decimal wartosc, string waluta)
+//         {
+//             this.wartosc = wartosc;
+//             this.waluta = waluta;
+//         }
+//
+//         public static bool operator == (Kwota left, Kwota right)
+//         {
+//             if (left is null || right is null)
+//                 return false;
+//
+//             if (left.waluta != right.waluta)
+//                 return false;
+//
+//             return left.wartosc == right.wartosc;
+//         }
+//
+//         public static bool operator !=(Kwota left, Kwota right)
+//         {
+//             return !(left == right);
+//         }
+//
+//         public static Kwota operator +(Kwota left, Kwota right)
+//         {
+//             if (left is null || right is null)
+//                 throw new ArgumentNullException("Kwota nie moze byc pusta");
+//
+//             if (left.waluta != right.waluta)
+//                 throw new ArgumentNullException("Nie mozna dodawac kwot o roznych walutach");
+//
+//             return new Kwota(left.wartosc + right.wartosc, left.waluta);
+//         }
+//     }
+// }
+
+
+// //====================zad 1
+// namespace Lab_05
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             var mathOperations = new MathOperations();
+//             var r1 = mathOperations.Add(1, 1);
+//             var r2 = mathOperations.Add(2d, 2d);
+//         }
+//     }
+//
+//     public class MathOperations
+//     {
+//         public int Add(int num1, int num2)
+//         {
+//             return num1 + num2;
+//         }
+//
+//         public double Add(double num1, double num2)
+//         {
+//             return num1 + num2;
+//         }
+//     }
+// }
+
+//====================zad 3
+namespace Lab_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c1 = new ComplexNum(5, 2);
+            var c2 = new ComplexNum(3, -7);
+
+            // var c3 = c1 + c2;
+            // var c3 = c1 - c2;
+            var c3 = c1 * c2;
+
+            Console.WriteLine($"Wynik: {c3}");
+        }
+    }
+
+    public class ComplexNum
+    {
+        public double Real { get; private set; }
+        public double Imaginary { get; private set; }
+
+        public ComplexNum(double real, double imaginary)
+        {
+            Real = real;
+            Imaginary = imaginary;
+        }
+
+        public static ComplexNum operator +(ComplexNum left, ComplexNum right)
+        {
+            var realVal = left.Real + right.Real;
+            var imVal = left.Imaginary + right.Imaginary;
+            return new ComplexNum(realVal, imVal);
+        }
+
+        public static ComplexNum operator -(ComplexNum left, ComplexNum right)
+        {
+            var realVal = left.Real - right.Real;
+            var imVal = left.Imaginary - right.Imaginary;
+            return new ComplexNum(realVal, imVal);
+        }
+
+        public static ComplexNum operator *(ComplexNum left, ComplexNum right)
+        {
+            double realVal = (left.Real * right.Real) - (left.Imaginary * right.Imaginary);
+            double imaginaryVal = (left.Real * right.Imaginary) + (left.Imaginary * right.Real);
+
+            return new ComplexNum(realVal, imaginaryVal);
+        }
+
+        public override string ToString()
+            => $"{Real} + {Imaginary}i";
+    }
+}
