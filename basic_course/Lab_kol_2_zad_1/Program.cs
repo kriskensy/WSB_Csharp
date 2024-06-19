53077fbee36364da9a241e02349bc37ebf9181b7
kriskensy <kriskensy@proton.me>
2024-06-19 21:09:38 +0200

    Test. Lab 4-7. Classes, interfaces, inhefitance, operator overloading...

---
+++ basic_course/Lab_kol_2_zad_1/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab_kol_2_zad_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cart cart = new Cart();
+
+            IProduct notebookDell = new Product("Notebook Dell", 1799.00m);
+            IProduct monitorMSI = new Product("Monitor MSI", 249.00m);
+            IProduct headsetJabra = new Product("Headset Jabra", 219.00m);
+
+            cart.AddProduct(notebookDell);
+            cart.AddProduct(monitorMSI);
+            cart.AddProduct(headsetJabra);
+
+            Console.WriteLine(cart);
+            Console.WriteLine($"-> Total price: {cart.CalculateTotal()}");
+
+            IProduct tabletSurface = new Product("Tablet Surface", 1299.00m);
+            IProduct mouseLogi = new Product("Mouse Logi", 99.00m);
+            IProduct keyboardLogi = new Product("Keyboard Logi", 79.00m);
+
+            cart = cart + tabletSurface;
+            cart = cart + mouseLogi;
+            cart = cart + keyboardLogi;
+            cart = cart - headsetJabra;
+
+            Console.WriteLine();
+
+            Console.WriteLine(cart);
+            Console.WriteLine($"-> Total price: {cart.CalculateTotal()}");
+        }
+    }
+
+    public interface IProduct
+    {
+        string Name { get; }
+        decimal Price { get; }
+    }
+
+    public interface ICart
+    {
+        void AddProduct(IProduct product);
+        void RemoveProduct(IProduct product);
+        decimal CalculateTotal();
+    }
+
+    public class Product : IProduct
+    {
+        public string Name { get; private set; }
+        public decimal Price { get; private set; }
+
+        public Product(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} - {1:C}", Name, Price);
+        }
+    }
+
+    public class Cart : ICart
+    {
+        private List<IProduct> cartProducts = new List<IProduct>();
+
+        public void AddProduct(IProduct product)
+        {
+            cartProducts.Add(product);
+        }
+
+        public void RemoveProduct(IProduct product)
+        {
+            cartProducts.Remove(product);
+        }
+
+        public decimal CalculateTotal()
+        {
+            decimal totalPrice = 0;
+            foreach (var product in cartProducts)
+            {
+                totalPrice += product.Price;
+            }
+
+            return totalPrice;
+        }
+
+
+        public static Cart operator +(Cart cart, IProduct product)
+        {
+            cart.AddProduct(product);
+            return cart;
+        }
+
+        public static Cart operator -(Cart cart, IProduct product)
+        {
+            cart.RemoveProduct(product);
+            return cart;
+        }
+
+        public override string ToString()
+        {
+            return string.Join("\n", cartProducts);
+        }
+    }
+}
+
+
+/*
+ * Stwórz aplikację do obsługi sklepu internetowego, która zawiera funkcjonalność dla produktów oraz koszyka zakupowego.
+ * Zadanie polega na zaprojektowaniu interfejsów oraz przeciążeniu operatorów dla klas związanych z produktami i koszykiem.
+
+Zdefiniuj interfejs "IProduct" z następującymi składowymi:
+◦ Właściwość "Name" (typ string) - przechowuje nazwę produktu
+◦ Właściwość "Price" (typ decimal) - przechowuje cenę produktu
+Następnie, zdefiniuj interfejs "ICart" z następującymi składowymi:
+◦ Metoda "AddProduct" - przyjmuje obiekt typu "IProduct" i dodaje go do koszyka
+◦ Metoda "RemoveProduct" - przyjmuje obiekt typu "IProduct" i usuwa go z koszyka
+◦ Metoda "CalculateTotal" - zwraca sumę cen wszystkich produktów w koszyku
+Stwórz klasę "Product", która implementuje interfejs "IProduct". Klasa powinna zawierać odpowiednie właściwości oraz konstruktor.
+Następnie, zdefiniuj klasę "Cart", która implementuje interfejs "ICart". Klasa powinna przechowywać listę produktów oraz zaimplementować metody interfejsu.
+Przeciąż operatory "+" i "-" dla klasy "Cart", tak aby umożliwić dodawanie i usuwanie produktów za pomocą tych operatorów.
+
+Zadania:
+
+Zaimplementuj interfejsy "IProduct" oraz "ICart" według powyższych specyfikacji.
+Stwórz kilka obiektów klasy "Product" i dodaj je do obiektu klasy "Cart". Wywołaj metodę "CalculateTotal" i wyświetl jej wynik.
+Przetestuj przeciążone operatory "+" i "-" dla obiektu klasy "Cart". Dodaj kilka produktów za pomocą operatora "+"
+oraz usuń jeden produkt za pomocą operatora "-". Wywołaj ponownie metodę "CalculateTotal" i wyświetl jej wynik.
+*/
---
+++ basic_course/Lab_kol_2_zad_2/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab_kol_2_zad_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegularUser user = new RegularUser("normal user", "123");
+            Moderator moderator = new Moderator("moderator", "456", new List<string> { "Posts", "News" });
+            Administrator admin = new Administrator("admin", "111", 2);
+
+            user.GetInfo();
+            moderator.GetInfo();
+            admin.GetInfo();
+        }
+    }
+
+    public class User
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public User(string login, string password)
+        {
+            Login = login;
+            Password = password;
+        }
+
+        public virtual void GetInfo()
+        {
+            Console.WriteLine($"Login: {Login}");
+        }
+    }
+
+    public class Administrator : User
+    {
+        public int AccessLevel { get; set; }
+
+        public Administrator(string login, string password, int accessLevel) : base(login, password)
+        {
+            AccessLevel = accessLevel;
+        }
+
+        public override void GetInfo()
+        {
+            base.GetInfo();
+            Console.WriteLine($"Admin, access Level: {AccessLevel}");
+        }
+    }
+
+    public class Moderator : User
+    {
+        public List<string> GrantedModules { get; set; }
+
+        public Moderator(string login, string password, List<string> grantedModules) : base(login, password)
+        {
+            GrantedModules = grantedModules;
+        }
+
+        public override void GetInfo()
+        {
+            base.GetInfo();
+            Console.WriteLine("Moderator, granted Modules: " + string.Join(", ", GrantedModules));
+        }
+    }
+
+    public class RegularUser : User
+    {
+        public RegularUser(string login, string password) : base(login, password)
+        {
+        }
+
+        public override void GetInfo()
+        {
+            base.GetInfo();
+            Console.WriteLine("Normal User");
+        }
+    }
+}
+
+/*
+ * Załóżmy, że masz do wykonania system rejestracji użytkowników w aplikacji. Użytkownicy mają różne role, takie jak
+ * Administrator, Moderator i Użytkownik. Zadanie polega na zaprojektowaniu hierarchii klas, które umożliwią zaimplementowanie tej funkcjonalności.
+
+ Zdefiniuj klasę bazową o nazwie "User" z następującymi polami:
+ ◦ Login (string) - przechowuje nazwę użytkownika
+ ◦ Password (string) - przechowuje hasło użytkownika
+Następnie, zdefiniuj klasę "Administrator", dziedziczącą po klasie "User", która posiada dodatkowe pole:
+◦ AccessLevel (int) - przechowuje poziom uprawnień administratora (np. 1 - regular, 2 - advanced itp.)
+Kolejna klasa, "Moderator", również dziedziczy po klasie "User" i posiada pola:
+◦ GrantedModules (List) - przechowuje listę obszarów, w których moderator ma uprawnienia do moderacji
+Na koniec, zdefiniuj klasę "RegularUser", dziedziczącą po klasie "User".
+
+Zadania:
+
+Zaimplementuj odpowiednie konstruktory dla klas "Administrator", "Moderator" i "RegularUser", które umożliwią ustawienie
+wartości pól klasy bazowej ("User") oraz dodatkowych pól specyficznych dla każdej klasy.
+Zaimplementuj metodę w klasie "User" o nazwie "GetInfo", która wyświetli na konsoli informacje o użytkowniku
+(np. login, poziom dostępu dla administratora, moduły itp.). Stwórz instancje obiektów dla wszystkich klas:
+"Administrator", "Moderator" i "RegularUser", i przetestuj działanie metody "GetInfo" dla każdego z obiektów.
+Oceniane będą poprawność dziedziczenia, implementacja konstruktorów, działanie metody "GetInfo" oraz podatność na błędy.
+*/
